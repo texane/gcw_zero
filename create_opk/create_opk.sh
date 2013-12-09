47339b1747e46c3479d75019e03c91ad60c71b43
texane <texane@gmail.com>
2013-12-08 23:02:31 -0600

    [ update ] icon defaults to black

--- create_opk/create_opk.sh
+++ create_opk/create_opk.sh
@@ -6,8 +6,12 @@ if [ -z $1 ]; then
 fi
 
 zip_path=$HOME/repo/roms/$1.zip
+png_name=$1
 png_path=$HOME/repo/roms/$1.png
-[ -e $png_path ] || png_path='black.png'
+if [ ! -e $png_path ]; then
+ png_name='black'
+ png_path='black.png'
+fi
 opk_path=$HOME/repo/gcw_zero/opk/$1.opk
 
 s='[Desktop Entry]\n'
@@ -17,7 +21,7 @@ s=$s'Exec=./fbasdl.dge ./'$1'.zip\n'
 s=$s'Terminal=true\n'
 s=$s'Type=Application\n'
 s=$s'StartupNotify=false\n'
-s=$s'Icon='$1'\n'
+s=$s'Icon='$png_name'\n'
 s=$s'Categories=games;\n'
 
 echo $s > /tmp/default.a320.desktop
